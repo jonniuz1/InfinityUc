e328ebe6d25ab58467a70d7af771f1e301d2783a
jonniuz1 <101719360+jonniuz1@users.noreply.github.com>
2023-02-20 23:51:43 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -9,7 +9,8 @@ BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 CHANNELS = env.list("CHANNELS")  # adminlar ro'yxati
 IP = env.str("ip")  # Xosting ip manzili
-
+SQLALCHEMY_DATABASE_URI = 'postgres://postgres:Asdfvcxz1@infinityucbotdb.cat1mvipi4nl.ap-northeast-1.rds.amazonaws.com:5432/InfinityucBot_DB'
+# SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:password@hostname:port/db_name'
 DB_USER = env.str("DB_USER")
 DB_PASS = env.str("DB_PASS")
 DB_NAME = env.str("DB_NAME")
